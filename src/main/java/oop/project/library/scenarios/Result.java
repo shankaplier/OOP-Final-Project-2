656522c6e987990be3129a7a6a72996f2c171c42
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-11-09 22:22:29 -0500

    Nicer interface for Result

--- src/main/java/oop/project/library/scenarios/Result.java
+++ src/main/java/oop/project/library/scenarios/Result.java
@@ -1,8 +1,34 @@
 package oop.project.library.scenarios;
 
+import java.util.function.Function;
+
 public sealed interface Result<T> {
 
-    record Success<T>(T value) implements Result<T> {}
-    record Failure<T>(String error) implements Result<T> {}
+    record Success<T>(T value) implements Result<T> {
+
+        @Override
+        public <O> Result<O> map(Function<T, O> f) {
+            return new Success<>(f.apply(value));
+        }
+
+        @Override
+        public <O> Result<O> bind(Function<T, Result<O>> f) {
+            return f.apply(value);
+        }
+    }
+    record Failure<T>(String error) implements Result<T> {
+        @Override
+        public <O> Result<O> map(Function<T, O> f) {
+            return new Failure<>(error);
+        }
+
+        @Override
+        public <O> Result<O> bind(Function<T, Result<O>> f) {
+            return new Failure<>(error);
+        }
+    }
+
+    public <O> Result<O> map(Function<T, O> f);
+    public <O> Result<O> bind(Function<T, Result<O>> f);
 
 }
