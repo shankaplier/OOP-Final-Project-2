7a1e3077539d22b28ec03ace54facbc198737d34
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-11-10 00:56:20 -0500

    Better variable name

--- src/main/java/oop/project/library/parsing/IntegerParser.java
+++ src/main/java/oop/project/library/parsing/IntegerParser.java
@@ -3,9 +3,9 @@ package oop.project.library.parsing;
 import oop.project.library.scenarios.Result;
 
 public class IntegerParser {
-    public static Result<Integer> parse(String unparsedLeft) {
+    public static Result<Integer> parse(String input) {
         try {
-            return new Result.Success<>(Integer.parseInt(unparsedLeft));
+            return new Result.Success<>(Integer.parseInt(input));
         } catch (Exception e) {
             return new Result.Failure<>(e.getMessage());
         }
