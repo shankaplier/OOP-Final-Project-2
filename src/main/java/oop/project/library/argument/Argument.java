d1cf656c4b7ef36d05835be34616f44f716457a8
TheShmill <21013808+TheShmill@users.noreply.github.com>
2024-12-12 19:36:17 -0500

    Argument javadoc

--- src/main/java/oop/project/library/argument/Argument.java
+++ src/main/java/oop/project/library/argument/Argument.java
@@ -6,6 +6,15 @@ import oop.project.library.parsing.Parser;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * @param <T> The type that the argument produces
+ * @param parser Converts an input string to a T
+ * @param validators An argument must pass every validator predicate
+ * @param name The name used to extract the value
+ * @param argumentType Whether the argument is named or positional
+ * @param optional Is the argument non-required?
+ * @param defaultValue Value to use if the argument isn't provided
+ */
 public record Argument<T> (Parser<T> parser, List<Validator<T>> validators, String name, ArgumentType argumentType, boolean optional, T defaultValue){
     public T run(String input) throws ValidateException, ParseException {
         T value = parser.parse(input);
